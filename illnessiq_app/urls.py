38242b02e04e84d99685aa4e96186acf0f1c0304
cod.r64 <prasaiaayus@gmail.com>
2025-05-05 17:15:07 +0545

    routing dashboard urls

--- illnessiq_app/urls.py
+++ illnessiq_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login',views.login, name='login'),
     path('signup', views.signup, name='signup'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('user/',views.user_dashboard, name='user_dahsboard')
+    path('admin/',views.admin_dashboard, name='admin_dahsboard')
 ]
