14ec7a691a201322088e2f320dfa8e36463ad2d2
cod.r64 <prasaiaayus@gmail.com>
2025-06-24 22:54:44 +0545

    added .env file to hide exposure

--- IllnessIQ/settings.py
+++ IllnessIQ/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +80,11 @@ WSGI_APPLICATION = 'IllnessIQ.wsgi.application'
 
 #Gmail SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'prasaiaayus@gmail.com'
-EMAIL_HOST_PASSWORD = 'vgzmtgeehgllvtix'
-
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
